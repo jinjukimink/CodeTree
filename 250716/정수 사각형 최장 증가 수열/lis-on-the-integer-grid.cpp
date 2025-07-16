d631f3d90df7a69fcf386153f158e10f5f13adb5
jinjukim_web <63224644+jinjukimink@users.noreply.github.com>
2025-07-16 17:08:40 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 97ms, Memory: 2MB, Status: Passed - Codetree

--- 250716/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 250716/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,94 +1,41 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int>>dp;
-vector<vector<int>>grid;
 int n;
-void input(){
-    cin>>n;
-    dp.resize(n,vector<int>(n+1,1));
-    grid.resize(n,vector<int>(n+1));
-    for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            cin>>grid[i][j];
+vector<vector<int>> grid;
+vector<vector<int>> dp;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+
+int dfs(int x, int y) {
+    if (dp[x][y] != 0) return dp[x][y];  // 이미 계산된 값이면 반환
+
+    dp[x][y] = 1;  // 자기 자신
+    for (int dir = 0; dir < 4; dir++) {
+        int nx = x + dx[dir], ny = y + dy[dir];
+        if (nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] > grid[x][y]) {
+            dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1);
+        }
+    }
+    return dp[x][y];
 }
 
-int main(){
-    input();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i-1>0 && grid[i][j]>grid[i-1][j]){
-                dp[i][j]=max(dp[i][j],dp[i-1][j]+1);
-            }
-            if(j-1>0 && grid[i][j]>grid[i][j-1]){
-                dp[i][j]=max(dp[i][j],dp[i][j-1]+1);
-            }
-            if(i+1<n && grid[i][j]>grid[i+1][j]){
-                dp[i][j]=max(dp[i][j],dp[i+1][j]+1);
-            }
-            if(j+1<n && grid[i][j]>grid[i][j+1]){
-                dp[i][j]=max(dp[i][j],dp[i][j+1]+1);
-            }
-        }
-    }
-    for(int i=n-1;i>=0;i--){
-        for(int j=n-1;j>=0;j--){
-            if(i-1>0 && grid[i][j]>grid[i-1][j]){
-                dp[i][j]=max(dp[i][j],dp[i-1][j]+1);
-            }
-            if(j-1>0 && grid[i][j]>grid[i][j-1]){
-                dp[i][j]=max(dp[i][j],dp[i][j-1]+1);
-            }
-            if(i+1<n && grid[i][j]>grid[i+1][j]){
-                dp[i][j]=max(dp[i][j],dp[i+1][j]+1);
-            }
-            if(j+1<n && grid[i][j]>grid[i][j+1]){
-                dp[i][j]=max(dp[i][j],dp[i][j+1]+1);
-            }
-        }
-    }
+int main() {
+    cin >> n;
+    grid.assign(n, vector<int>(n));
+    dp.assign(n, vector<int>(n, 0));
 
-    for(int i=0;i<n;i++){
-        for(int j=n-1;j>=0;j--){
-            if(i-1>0 && grid[i][j]>grid[i-1][j]){
-                dp[i][j]=max(dp[i][j],dp[i-1][j]+1);
-            }
-            if(j-1>0 && grid[i][j]>grid[i][j-1]){
-                dp[i][j]=max(dp[i][j],dp[i][j-1]+1);
-            }
-            if(i+1<n && grid[i][j]>grid[i+1][j]){
-                dp[i][j]=max(dp[i][j],dp[i+1][j]+1);
-            }
-            if(j+1<n && grid[i][j]>grid[i][j+1]){
-                dp[i][j]=max(dp[i][j],dp[i][j+1]+1);
-            }
-        }
-    }
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> grid[i][j];
 
-    for(int i=n-1;i>=0;i--){
-        for(int j=0;j<n;j++){
-            if(i-1>0 && grid[i][j]>grid[i-1][j]){
-                dp[i][j]=max(dp[i][j],dp[i-1][j]+1);
-            }
-            if(j-1>0 && grid[i][j]>grid[i][j-1]){
-                dp[i][j]=max(dp[i][j],dp[i][j-1]+1);
-            }
-            if(i+1<n && grid[i][j]>grid[i+1][j]){
-                dp[i][j]=max(dp[i][j],dp[i+1][j]+1);
-            }
-            if(j+1<n && grid[i][j]>grid[i][j+1]){
-                dp[i][j]=max(dp[i][j],dp[i][j+1]+1);
-            }
-        }
-    }
-    int ans=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            ans=max(dp[i][j],ans);
-        }
-    }
-    cout<<ans;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            ans = max(ans, dfs(i, j));
+
+    cout << ans << endl;
     return 0;
 }
