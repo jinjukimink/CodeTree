78e577e88611bfe07f375322ced800fd4ee5581d
jinjukim_web <63224644+jinjukimink@users.noreply.github.com>
2025-07-16 17:05:42 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 76ms, Memory: 1MB, Status: Wrong Answer - Codetree

--- 250716/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 250716/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -8,13 +8,12 @@ vector<vector<int>>grid;
 int n;
 void input(){
     cin>>n;
-    dp.resize(n,vector<int>(n,1));
-    grid.resize(n,vector<int>(n));
+    dp.resize(n,vector<int>(n+1,1));
+    grid.resize(n,vector<int>(n+1));
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
             cin>>grid[i][j];
 }
-
 
 int main(){
     input();
@@ -50,6 +49,41 @@ int main(){
             }
         }
     }
+
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=0;j--){
+            if(i-1>0 && grid[i][j]>grid[i-1][j]){
+                dp[i][j]=max(dp[i][j],dp[i-1][j]+1);
+            }
+            if(j-1>0 && grid[i][j]>grid[i][j-1]){
+                dp[i][j]=max(dp[i][j],dp[i][j-1]+1);
+            }
+            if(i+1<n && grid[i][j]>grid[i+1][j]){
+                dp[i][j]=max(dp[i][j],dp[i+1][j]+1);
+            }
+            if(j+1<n && grid[i][j]>grid[i][j+1]){
+                dp[i][j]=max(dp[i][j],dp[i][j+1]+1);
+            }
+        }
+    }
+
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<n;j++){
+            if(i-1>0 && grid[i][j]>grid[i-1][j]){
+                dp[i][j]=max(dp[i][j],dp[i-1][j]+1);
+            }
+            if(j-1>0 && grid[i][j]>grid[i][j-1]){
+                dp[i][j]=max(dp[i][j],dp[i][j-1]+1);
+            }
+            if(i+1<n && grid[i][j]>grid[i+1][j]){
+                dp[i][j]=max(dp[i][j],dp[i+1][j]+1);
+            }
+            if(j+1<n && grid[i][j]>grid[i][j+1]){
+                dp[i][j]=max(dp[i][j],dp[i][j+1]+1);
+            }
+        }
+    }
+    
     int ans=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
